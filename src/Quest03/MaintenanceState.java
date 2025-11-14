1e21af0202ab5c067665ad202bd1a9b2f07c0629
Otavio Caetano Ribeiro <144289873+BUR1T1@users.noreply.github.com>
2025-11-14 14:20:08 -0300

    Ajuste de gramática

--- src/Quest03/MaintenanceState.java
+++ src/Quest03/MaintenanceState.java
@@ -3,7 +3,7 @@ package Quest03;
 public class MaintenanceState implements ReactorState {
     @Override
     public void handle(ReactorContext context) {
-        System.out.println("🔧 Modo manutenção ativo — ignorando regras normais.");
+        System.out.println(" Modo manutenção ativo — ignorando regras normais.");
     }
     public String getName() { return "MANUTENCAO"; }
 }
